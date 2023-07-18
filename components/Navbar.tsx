fa946340ac4d82d085f3be01ccf971a6eb356b7b
SergiusLo <slobojko21@gmail.com>
2023-07-18 13:54:20 +0300

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { NavLinks } from "@/constans";
+import Image from "next/image";
+import Link from "next/link";
+import AuthProviders from "./AuthProviders";
 
 const NavBar = () => {
+  const session = {};
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image
+            src="./logo.svg"
+            width={115}
+            height={43}
+            alt="Shoot-app"
+            loading="lazy"
+          />
+        </Link>
+        <ul className="xl:flex hidden text small gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
