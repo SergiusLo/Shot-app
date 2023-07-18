266ae2a32c3868c0f595a2edff7dfd6899e8e6a5
SergiusLo <slobojko21@gmail.com>
2023-07-18 11:52:45 +0300

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react'
 
 const Home: FC = () => {
-  return <div>Home</div>
+  return <section className='flex-start flex-col padding mb-16'>
+   <h1>Categories</h1>
+   <h1>Posts</h1>
+   <h1>LoadMore</h1>
+  </section>
 }
 
 export default Home
