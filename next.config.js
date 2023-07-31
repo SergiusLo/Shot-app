1689131837d78b6e426bb95300ba8a5c7bf2c2ca
SergiusLo <slobojko21@gmail.com>
2023-08-01 00:03:47 +0300

    next.config for debug deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com","res.cloudinary.com"],
+    domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
   },
   experimental: {
     serverComponentsExternalPackages: ["cloudinary", "graphql-request"],
   },
+  reactStrictMode: true,
+  debug: true,
 };
 
 module.exports = nextConfig;
