2151c01f2b7df669211ecaf1695826f276f48746
SergiusLo <slobojko21@gmail.com>
2023-08-01 15:12:21 +0300

    delete dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -24,9 +24,7 @@ type ProjectSearch = {
   };
 };
 
-  export const dynamic = 'force-dynamic'
-  export const dynamicParams = true
-  export const revalidate = 0
+
 
 const Home = async ({ searchParams: { category, endcursor } }: Props) => {
   
